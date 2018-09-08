bc10addca82a549104f0e855a8910b89940d0175
Vaibhav Setia <vaibhavwimpsta@gmail.com>
2018-09-08 13:54:06 +0530

    Redirected to home.php with sid and password

--- stulogin.php
+++ stulogin.php
@@ -1,6 +1,6 @@
 <?php
 require_once("config.php");
-//error_reporting(0);
+error_reporting(0);
 ?>
 <!DOCTYPE HTML>
 <body>
@@ -10,7 +10,7 @@ require_once("config.php");
 
   <div class="form-group">
     <label for="exampleInputEmail1">SID</label>
-    <input type="text" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter SID">
+    <input type="text" name="sid" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter SID">
    <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>-->
   </div>
   <div class="form-group">
@@ -18,8 +18,8 @@ require_once("config.php");
     <input type="password" name="pass" class="form-control" id="exampleInputPassword1" placeholder="Password">
   </div>
   <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">forget password come here</label>
+    <!--<input type="checkbox" class="form-check-input" id="exampleCheck1">-->
+    <a href="forgot_password.php">Forgot Password?</a>
   </div>
   <button type="buttonx" name="submit" class="btn btn-primary">Submit</button>
 </form>
@@ -30,12 +30,12 @@ require_once("config.php");
 <?php
 if($_GET['mode'] == "check")
 {
-	$query = mysqli_fetch_row(mysqli_query($con, "select count(*) from student where sid='".$_POST['email']."' and password='".$_POST['pass']."'"));
+	$query = mysqli_fetch_row(mysqli_query($con, "select count(*) from student where sid='".$_POST['sid']."' and password='".$_POST['pass']."'"));
 	if($query[0] == 1)
 	{
 		echo "Login successful";
 		$query = mysqli_query($con, "select sid,password from student");
-		header("location: home.php?teacher=".$teacher."&password=".$password);
+		header("location: home.php?sid=".$_POST['sid']."&password=".$_POST['pass']);
 		/*while($row = mysqli_fetch_row($query))
 		{
 			echo $row[0]. " " . $row[1]. "<br>";
